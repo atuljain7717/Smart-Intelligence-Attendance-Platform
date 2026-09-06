43de430bc1c2f5fe0e60ed72f2a1243487cdff63
Atul jain <atulmhaiskar1998@gmail.com>
2026-09-06 16:06:33 +0530

    Update mobile desktop layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import App from "./App";
 import "./index.css";
 import "./App.css";
 
-import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
